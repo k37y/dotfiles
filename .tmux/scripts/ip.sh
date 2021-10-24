277e98dafb8a16a2a8e21961e4874ad5747a60c6
Vinu K <kevy.vinu@gmail.com>
2021-10-24 13:21:22 +0530

    Added changes in tmux status bar

--- .tmux/scripts/ip.sh
+++ .tmux/scripts/ip.sh
@@ -1,8 +1,11 @@
 while true; do
 	if nc -zw1 google.com 443 2>/dev/null; then
 		curl -s -w '\n' --connect-timeout 1 https://ifconfig.me 2>/dev/null
+		sleep 3
+		ip r | grep '^default' | cut -d' ' -f3 2>/dev/null
+		sleep 3
 	else
 		echo offline
+		sleep 1
 	fi
-	sleep 5
 done

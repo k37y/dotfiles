4cff8a802fd874d31893b4d34c72aff9ef2c4c2c
Vinu K <kevy.vinu@gmail.com>
2021-10-26 11:22:18 +0530

    Added changes to tmux ip.sh

--- .tmux/scripts/ip.sh
+++ .tmux/scripts/ip.sh
@@ -2,7 +2,7 @@ while true; do
 	if nc -zw1 google.com 443 2>/dev/null; then
 		curl -s -w '\n' --connect-timeout 1 https://ifconfig.me | awk '{printf "%.3d.%.3d.%.3d.%.3d\n",$1,$2,$3,$4}' FS=. 2>/dev/null
 		sleep 3
-		ip r | grep '^default' | cut -d' ' -f3 | awk '{printf "%.3d.%.3d.%.3d.%.3d\n",$1,$2,$3,$4}' FS=. 2>/dev/null
+		ip a s $(ip r | grep default | awk '{print $5}') | grep inet | awk '{print $2}' | cut -d'/' -f1 | awk '{printf "%.3d.%.3d.%.3d.%.3d\n",$1,$2,$3,$4}' FS=. 2>/dev/null
 		sleep 3
 	else
 		echo offline

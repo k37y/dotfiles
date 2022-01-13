0b14b189f0f391b3b8c8c985e9bf3fe1a3e5f54a
Vinu K <kevy.vinu@gmail.com>
2022-01-13 12:48:39 +0530

    Update ip.sh

--- .tmux/scripts/ip.sh
+++ .tmux/scripts/ip.sh
@@ -1,6 +1,6 @@
 while true; do
 	if nc -zw1 google.com 443 2>/dev/null; then
-		curl -s -w '\n' --connect-timeout 1 https://ifconfig.me | awk '{printf "%.3d.%.3d.%.3d.%.3d\n",$1,$2,$3,$4}' FS=. 2>/dev/null
+		curl -s -w '\n' --connect-timeout 1 ifconfig.me | awk '{printf "%.3d.%.3d.%.3d.%.3d\n",$1,$2,$3,$4}' FS=. 2>/dev/null
 		sleep 3
 		ip a s $(ip r | grep default | awk '{print $5}') | grep inet | awk '{print $2}' | cut -d'/' -f1 | awk '{printf "%.3d.%.3d.%.3d.%.3d\n",$1,$2,$3,$4}' FS=. 2>/dev/null
 		sleep 3
